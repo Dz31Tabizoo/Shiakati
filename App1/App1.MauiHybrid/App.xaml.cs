1175d1905f7ad1cd57e6b0c3d8ef68224d80d82b
Rafik <rafik-2015@live.fr>
2026-04-22 16:59:56 +0100

    UI TESTS

---
+++ App1/App1.MauiHybrid/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace App1.MauiHybrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "App1" };
+        }
+    }
+}
---
+++ App1/App1.MauiHybrid/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace App1.MauiHybrid
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
