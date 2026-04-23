c705ed4a142dc8350bbe90f95553d6beefbbe8da
Rafik <rafik-2015@live.fr>
2026-04-23 10:35:30 +0100

    add MainView

---
+++ Shiakati/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Shiakati.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private object _currentView;
+
+        public MainViewModel()
+        {
+            // set a default 
+            //CurrentView = new DashboardViewModel();
+        }
+        [RelayCommand]
+        private void NavigateToDashboard()
+        {
+            CurrentView = new DashboardViewModel();
+        }
+         [RelayCommand]
+        private void NavigateToSettings()
+        {
+            CurrentView = new SettingsViewModel();
+        } 
+        [RelayCommand]
+        private void NavigateToPOS()
+        {
+            CurrentView = new POSViewModel();
+        }
+    }
+}
