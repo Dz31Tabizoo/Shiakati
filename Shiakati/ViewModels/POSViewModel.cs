5ec6f860efa3dcea740a341135d2822c85566457
TaBiZoo <rafik-2015@live.fr>
2026-04-24 19:42:06 +0100

    add PosView + PosContainer Views and ViewModels

---
+++ Shiakati/ViewModels/POSViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Shiakati.ViewModels
+{
+    public partial class POSViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _tabName;
+        //add panier ICI 
+        // public ObservableCollection<SaleItem> Panier { get; set; } = new();
+
+        public POSViewModel(string name)
+        {
+            TabName = name;
+        }
+        
+    }
+}
---
+++ Shiakati/ViewModels/PosContainerViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Shiakati.ViewModels
+{
+    public partial class PosContainerViewModel : ObservableObject
+    {
+        public ObservableCollection<POSViewModel> PosTabs { get; set; } = new();
+
+        [ObservableProperty]
+        private POSViewModel _selectedTab;
+
+        private int _tabCounter = 1;
+
+        public PosContainerViewModel()
+        {
+            // Initialize with one tab
+            AddNewTab();
+        }
+        [RelayCommand]
+        private void AddNewTab()
+        {
+            var newTab = new POSViewModel($"Ticket #{_tabCounter++}");
+            PosTabs.Add(newTab);
+            SelectedTab = newTab;
+        }
+
+        [RelayCommand]
+        private void CloseTab(POSViewModel tabToClose)
+        {
+            if (tabToClose != null && PosTabs.Contains(tabToClose))
+            {
+                PosTabs.Remove(tabToClose);
+                if (SelectedTab == tabToClose)
+                {
+                    SelectedTab = PosTabs.Count > 0 ? PosTabs[0] : null;
+                }
+            }
+        }
+    }
+}
