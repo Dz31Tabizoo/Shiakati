59b5758ce924ffe1fa7982e3d115444fd072ee7b
Rafik <rafik-2015@live.fr>
2026-06-04 17:43:44 +0100

    released

--- Shiakati/ViewModels/MainViewModel.cs
+++ Shiakati/ViewModels/MainViewModel.cs
@@ -8,6 +8,7 @@ using Shiakati.Services.Interfaces;
 using Shiakati.Views;
 using System;
 using System.Collections.ObjectModel;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 
 namespace Shiakati.ViewModels
@@ -16,8 +17,8 @@ namespace Shiakati.ViewModels
     {
         private readonly IAuthenticationClientService _authService;
         private readonly ICacheService _cacheService;
-        [ObservableProperty]
-        private object _currentView;
+        [ObservableProperty] private object? _currentView;
+        [ObservableProperty] private string? _currentViewTitle;
 
         // On garde les instances des ViewModels pour ne pas les recréer à chaque click
 
@@ -30,10 +31,10 @@ namespace Shiakati.ViewModels
         public ClientListViewModel ClientList { get; }
         public SettingsViewModel Settings { get; }
 
-        public MainViewModel(ICacheService cacheService ,
+        public MainViewModel(ICacheService cacheService,
                             IAuthenticationClientService authService,
-                            PosContainerViewModel posContainer, 
-                            StockViewModel stockViewModel, 
+                            PosContainerViewModel posContainer,
+                            StockViewModel stockViewModel,
                             SalesHistoryViewModel salesHistory,
                             SettingsViewModel settingsViewModel,
                             StockMovementsViewModel stockMovementsViewModel,
@@ -47,6 +48,7 @@ namespace Shiakati.ViewModels
             Settings = settingsViewModel;
             // Vue par défaut au démarrage
             CurrentView = PosContainer;
+            CurrentViewTitle = "Point de Vente";
             ClientList = clientList;
 
             WeakReferenceMessenger.Default.Register<NavigateToPosMessage>(this, (r, m) =>
@@ -55,24 +57,37 @@ namespace Shiakati.ViewModels
             });
 
         }
-        
+
         [RelayCommand] private async Task NavigateToStock()
         {
             CurrentView = Stock;
+            CurrentViewTitle = "Stock";
             await Stock.LoadInitialDataAsync(true);
         }
         [RelayCommand] private void NavigateToPOS()
         {
+            if (CurrentView == PosContainer) return;
             CurrentView = PosContainer;
+            CurrentViewTitle = "Point de Vente";
             PosContainer.SelectedTab?.LoadProductsAsync();
         }
-        [RelayCommand] private void NavigateToSalesHistory() => CurrentView = SalesHistory;
+        [RelayCommand] private void NavigateToSalesHistory()
+        {
+            CurrentView = SalesHistory;
+            CurrentViewTitle = "Historique des Ventes";
+
+        }
         [RelayCommand] private void ExitApplication()
         {
             // Logique pour fermer l'application
             System.Windows.Application.Current.Shutdown();
         }
-        [RelayCommand] private void NavToSettings() => CurrentView = Settings;
+        [RelayCommand] private void NavToSettings()
+        {
+            CurrentView = Settings;
+            CurrentViewTitle = "Paramètres";
+
+        }
         [RelayCommand] private void Logout()
         {
             _authService.Logout();
@@ -87,8 +102,20 @@ namespace Shiakati.ViewModels
             App.Current.MainWindow?.Hide();   // now hides the actual main menu
             loginView.Show();
         }
-        [RelayCommand] private void NavigateToStockMovements() => CurrentView = StockMovements;
-        [RelayCommand] private void NavigateToClients() => CurrentView = ClientList;
-
+        [RelayCommand] private void NavigateToStockMovements()
+        {
+            CurrentView = StockMovements;
+            CurrentViewTitle = "Mouvements de Stock";
+        }
+        [RelayCommand] private void NavigateToClients()
+        {
+            CurrentView = ClientList;
+            CurrentViewTitle = "Clients C/V";
+        }
+        [RelayCommand] private void CloseCurrentView()
+        {
+            CurrentView = null;
+            CurrentViewTitle = null;  // also hides the header bar
+        }
     }
 }
