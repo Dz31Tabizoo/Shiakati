e07417eee08e9cff8d8ec53b2b0ec034d93b4a01
TaBiZoo <rafik-2015@live.fr>
2026-06-14 14:45:46 +0100

    add null to vis

---
+++ Shiakati/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Shiakati.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool invert = parameter?.ToString() == "invert";
+            bool isNull = value == null;
+            return (invert ? !isNull : isNull) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
